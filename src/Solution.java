961aa9aab354e40c1a1e434cf188415b86b76151
frank <franktranvantu@gmail.com>
2023-05-22 21:16:48 +0700

    1. Two Sum

---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Solution {
+  public static void main(String[] args) {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    int[] output = twoSum(nums, target);
+    System.out.println(Arrays.toString(output));
+  }
+
+  public static int[] twoSum(int[] nums, int target) {
+    int[] output = new int[2];
+    for (int i = 0; i < nums.length - 1; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          output[0] = i;
+          output[1] = j;
+        }
+      }
+    }
+    return output;
+  }
+}
