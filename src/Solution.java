326e3c063b3d8dda3299a4df583a40c7d9592a6f
Tu Tran <TTran.larion@absolute.com>
2023-05-24 13:18:46 +0700

    Update

--- src/Solution.java
+++ src/Solution.java
@@ -11,79 +11,52 @@ public class Solution {
   }
 
   public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    ListNode l1Temp = new ListNode(l1.getVal());
-    ListNode l2Temp = new ListNode(l2.getVal());
-    while (l1.getNext() != null) {
-      l1 = l1.getNext();
-      l1Temp = new ListNode(l1.getVal(), l1Temp);
+    ListNode dummy = new ListNode(0);
+    ListNode current = dummy;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int sum = 0;
+      if (l1 != null) {
+        sum = l1.val;
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        sum = sum + l2.val;
+        l2 = l2.next;
+      }
+      sum = sum + carry;
+      carry = sum / 10;
+      current.next = new ListNode(sum % 10);
+      current = current.next;
     }
-    while (l2.getNext() != null) {
-      l2 = l2.getNext();
-      l2Temp = new ListNode(l2.getVal(), l2Temp);
-    }
-    int num1 = 0;
-    while (l1Temp.getNext() != null) {
-      num1 = num1 * 10 + l1Temp.getVal();
-      l1Temp = l1Temp.getNext();
-    }
-    num1 = num1 * 10 + l1Temp.getVal();
-
-    int num2 = 0;
-    while (l2Temp.getNext() != null) {
-      num2 = num2 * 10 + l2Temp.getVal();
-      l2Temp = l2Temp.getNext();
-    }
-    num2 = num2 * 10 + l2Temp.getVal();
-
-    int sum = num1 + num2;
-    ListNode outputTemp = new ListNode(sum % 10);
-    while (sum / 10 > 0) {
-      sum = sum / 10;
-      outputTemp = new ListNode(sum % 10, outputTemp);
-    }
-    ListNode output = new ListNode(outputTemp.getVal());
-    while (outputTemp.getNext() != null) {
-      outputTemp = outputTemp.getNext();
-      output = new ListNode(outputTemp.getVal(), output);
-    }
-    return output;
+    return dummy.next;
   }
 }
 
 class ListNode {
-  private int val;
-  private ListNode next;
+  int val;
+  ListNode next;
 
-  public ListNode() {
+  ListNode() {}
 
-  }
-
-  public ListNode(int val) {
+  ListNode(int val) {
     this.val = val;
   }
 
-  public ListNode(int val, ListNode next) {
+  ListNode(int val, ListNode next) {
     this.val = val;
     this.next = next;
   }
 
-  public int getVal() {
-    return val;
-  }
-
-  public ListNode getNext() {
-    return next;
-  }
-
   @Override
-  public String toString() {
+  String toString() {
     List<Integer> list = new ArrayList<>();
     ListNode listNode = this;
-    while (listNode.getNext() != null) {
-      list.add(listNode.getVal());
-      listNode = listNode.getNext();
+    while (listNode.next != null) {
+      list.add(listNode.val);
+      listNode = listNode.next;
     }
-    list.add(listNode.getVal());
+    list.add(listNode.val);
     return list.toString();
   }
 }
